593d7d4d7ce5814e76bb6bd5d94e5e1d351ded0a
luansilvadb <contato@luansilva.com.br>
2026-07-31 17:16:30 -0300

    1.3.26

--- dist/index.cjs
+++ dist/index.cjs
@@ -12411,7 +12411,7 @@ var init_package = __esm({
   "package.json"() {
     package_default = {
       name: "@luansilvadb/conductor",
-      version: "1.3.25",
+      version: "1.3.26",
       description: "Conductor - Spec Driven Development",
       type: "module",
       bin: {
